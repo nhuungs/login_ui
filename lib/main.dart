a539b0667aba8292aabe45e2197573a9f702fe0b
nhuungs <lainhung2302@gmail.com>
2021-12-28 16:50:25 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-
       home: LoginScreen(),
-
-
     );
   }
 }
